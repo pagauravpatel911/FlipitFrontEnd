4538127eb29b78652e554ef20ec964b4f71bd785
Gaurav <gaurav.patel@asmadiya.com>
2022-09-01 09:27:42 +0530

    create Dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Loginpage from './components/LoginPage/LoginPage';
 import HomePage from "./components/HomePage/HomePage";
 import Header from './components/Header/Header';
 import{BrowserRouter,Routes,Route} from "react-router-dom"
+import NavBar from './components/Header/Navbar/Navbar';
+import Dashboard from './components/DashBoard/DashBoard';
+import UploadedDocs from './components/UploadDocs/UploadDocs';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
      <Route path="/" element={<Loginpage/>} />
      <Route path="/home" element={<HomePage/>} />
      <Route path="/header" element={<Header/>} />
+     <Route path="/navbar" element={<NavBar/>}  />
+     <Route path="/dashboard" element={<Dashboard/>}   />
+     <Route path="/uploadDocs" element={<UploadedDocs/>}   />
    </Routes>
    </BrowserRouter>
     </div>
